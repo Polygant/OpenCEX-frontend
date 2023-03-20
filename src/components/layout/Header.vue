27ba1014d5ccf89c56cf76a492c5b146d6de26b7
botjoker <botjoker@gmail.com>
2023-03-20 15:51:17 +0300

    Fix themes

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -110,8 +110,8 @@
             width="10"
             height="10"
             viewBox="0 0 21 21"
-            @click.stop="toggleIsSettingsOpen"
             xmlns="http://www.w3.org/2000/svg"
+            @click.stop="toggleIsSettingsOpen"
           >
             <g id="" transform="translate(-254 -232)">
               <rect
@@ -210,7 +210,22 @@
         :class="{ dark: theme === 'dark' }"
         @click="changeTheme"
       >
-        <img src="/public/img/theme-dark.svg" />
+        <svg
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <g stroke="none" stroke-width="1" fill="#FFFFFF" fill-rule="evenodd">
+            <g id="themeSelector" fill="#212121" fill-rule="nonzero">
+              <path
+                d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20 L12,4 C16.418278,4 20,7.581722 20,12 C20,16.418278 16.418278,20 12,20 Z"
+              ></path>
+            </g>
+          </g>
+        </svg>
       </div>
     </div>
   </header>
