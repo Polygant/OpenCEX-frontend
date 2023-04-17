21ef0d1689580ed0520dd6af351d5f4628481371
botjoker <botjoker@gmail.com>
2023-04-17 11:31:43 +0300

    Fix scroll

--- src/mixins/getCoolTrade.js
+++ src/mixins/getCoolTrade.js
@@ -16,37 +16,55 @@ export default {
     getCutted(val) {
       return Math.round((val + Number.EPSILON) * 100) / 100;
     },
-    getCoolTrade(sum, cur) {
-      sum = typeof sum !== "undefined" ? parseFloat(sum) : 0;
-      let result = sum;
-
-      if (
-        cur &&
-        this.isFiat(cur) &&
-        ["BTC", "ETH", "BCH"].includes(this.currentBaseCurrency)
-      ) {
-        if (sum >= 100) {
-          result = floor10(sum, -2).toFixed(2);
-        } else {
-          result = floor10(sum, -4).toFixed(4);
+    countZerosAfterDecimal(number) {
+      const decimalPart = number.toString().split(".")[1];
+      let zeroCount = 0;
+      if (decimalPart > 0) {
+        for (let i = 0; i < decimalPart.length; i++) {
+          if (decimalPart[i] === "0") {
+            zeroCount++;
+          } else {
+            break;
+          }
         }
-      } else if (cur && this.isFiat(cur)) {
-        if (sum >= 10000) {
-          result = floor10(sum, 0).toFixed(0);
-        } else if (sum >= 1000) {
-          result = floor10(sum, -1).toFixed(1);
-        } else if (sum >= 100) {
-          result = floor10(sum, -2).toFixed(2);
-        } else if (sum >= 10) {
-          result = floor10(sum, -3).toFixed(3);
-        } else {
-          result = floor10(sum, -4).toFixed(4);
-        }
-      } else {
-        result = floor10(sum, -8).toFixed(8);
       }
+      return zeroCount;
+    },
+    getCoolTrade(sum, cur) {
+      if (sum < 1 && this.countZerosAfterDecimal(sum) > 3) {
+        return floor10(sum, -8).toFixed(8);
+      } else {
+        sum = typeof sum !== "undefined" ? parseFloat(sum) : 0;
+        let result = sum;
 
-      return result;
+        if (
+          cur &&
+          this.isFiat(cur) &&
+          ["BTC", "ETH", "BCH"].includes(this.currentBaseCurrency)
+        ) {
+          if (sum >= 100) {
+            result = floor10(sum, -2).toFixed(2);
+          } else {
+            result = floor10(sum, -4).toFixed(4);
+          }
+        } else if (cur && this.isFiat(cur)) {
+          if (sum >= 10000) {
+            result = floor10(sum, 0).toFixed(0);
+          } else if (sum >= 1000) {
+            result = floor10(sum, -1).toFixed(1);
+          } else if (sum >= 100) {
+            result = floor10(sum, -2).toFixed(2);
+          } else if (sum >= 10) {
+            result = floor10(sum, -3).toFixed(3);
+          } else {
+            result = floor10(sum, -4).toFixed(4);
+          }
+        } else {
+          result = floor10(sum, -8).toFixed(8);
+        }
+
+        return result;
+      }
     },
   },
 };
